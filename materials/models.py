7c8e880b40f82678c494093a6c06d86a3e5c6a7b
Ekateryna <gorbunovakatja530@gmail.com>
2025-09-15 21:40:16 +0300

    commit 32_1

--- materials/models.py
+++ materials/models.py
@@ -48,3 +48,30 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        verbose_name="Пользователь",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="subscriptions",
+        verbose_name="Курс",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        unique_together = [
+            "user",
+            "course",
+        ]  # Один пользователь может подписаться на курс только один раз
+
+    def __str__(self):
+        return f"{self.user.email} - {self.course.name}"
--- materials/urls.py
+++ materials/urls.py
@@ -8,4 +8,5 @@ router.register(r'courses', CourseViewSet)
 urlpatterns = [
                   path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
                   path('lessons/<int:pk>/', LessonDetailView.as_view(), name='lesson-detail'),
-              ] + router.urls
+                  path("subscriptions/", SubscriptionView.as_view(), name="subscription"),
+                  ] + router.urls
